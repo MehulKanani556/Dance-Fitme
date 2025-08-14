ad51e4b5eafdffdea0bfb234dd4a8ab0ef606d0f
DarshanBackend <darshan1.kalathiyainfotech@gmail.com>
2025-08-14 11:35:26 +0530

    image changes

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 import express from 'express';
-import cors from 'cors';
 import { connectDB } from './src/config/db.js';
 import indexRouter from './src/routes/indexRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// ✅ Fix __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
-  credentials: true
-}));
+
+// ✅ Serve public folder correctly
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// Routes
+app.use("/api", indexRouter);
 
-// router
-app.use("/api", indexRouter)
-
-
-// Connect to database
+// DB connection
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,'0.0.0.0',() => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
